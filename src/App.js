6e1dfe35a5524e4ddc2a08a7add489ee5b3eec34
Omar_asc <initialshoot@outlook.com>
2022-06-20 13:33:33 -0500

    feat: add the buttons for the calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const setNumbers = () => {
+
+    const numbers = [];
+
+    for (let i = 1; i < 10; i++) {
+
+      numbers.push(
+        <button key={i}>{i}</button>
+      )
+      
+    }
+
+    return numbers;
+
+  };
+
+
   return (
+
     <div className="App">
+    
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <a>
+          Calculadora
         </a>
+
+        <div className="calculator">
+
+          <div className="display">
+          
+            <span>(0)</span>0
+
+          </div>
+
+          <div className="symbols">
+          
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+
+            <button>AC</button>
+
+          </div>
+
+          <div className="numbers">
+
+            { setNumbers() }
+
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          
+          </div>
+
+        </div>
+
       </header>
+
     </div>
+
   );
 }
 
